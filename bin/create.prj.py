14a8a74075d2bef23a520bf8b650a1e3e59c887f
Anton Bugreev <anton@bugreev.ru>
2015-05-07 12:53:07 +0600

    finish with create.prj.py

--- bin/create.prj.py
+++ bin/create.prj.py
@@ -10,44 +10,64 @@ import sys
 from subprocess import call
 import os
 
+## checks on right argumnets
 if '-h' in sys.argv or '--help' in sys.argv:
 	print __doc__
 	sys.exit(1)
 
+if not (2 <= len(sys.argv) <= 3):
+	print "Invalid arguments"
+	print __doc__
+	sys.exit(1)
+
+if len(sys.argv) == 3 and not(sys.argv[1] == '-v5'):
+	print "Invalid arguments"
+	print __doc__
+	sys.exit(1)
+	
+if len(sys.argv) == 2 and sys.argv[1] == '-v5':
+	print "Invalid arguments"
+	print __doc__
+	sys.exit(1)
+
+## save project name
 if len(sys.argv) == 2:
-	## go to current dir with script
-	os.chdir(os.path.dirname(sys.argv[0]))
-
-	## create variables
 	prj_name = sys.argv[1]
-	relative_prj_host = "../.nginx/etc/nginx/hosts/%s.conf" % prj_name
+	v5 = 0
+elif len(sys.argv) == 3:
+	prj_name = sys.argv[2]
+	v5 = 1
 
-	## exit if virtual host already exists
-	if os.path.isfile(relative_prj_host):
-		print "prj host %s already exists! Exiting.." % prj_name
-		sys.exit(2)
+### main
+## go to current dir with script
+os.chdir(os.path.dirname(sys.argv[0]))
 
-	## create nginx config
+## create variables
+relative_prj_host = "../.nginx/etc/nginx/hosts/%s.conf" % prj_name
+
+## exit if virtual host already exists
+if os.path.isfile(relative_prj_host):
+	print "prj host %s already exists! Exiting.." % prj_name
+	sys.exit(2)
+
+## create nginx config
+if v5 == 1:
+	document_root = "../www/%s/www/" % prj_name
+	s = open('../.nginx/etc/nginx/hosts/template-v5-conf', 'r').read()
+else:
+	document_root = "../www/%s/" % prj_name
 	s = open('../.nginx/etc/nginx/hosts/template-conf', 'r').read()
-	s = s.replace('PRJ_NAME', prj_name)
-	f = open(relative_prj_host, 'w')
-	f.write(s)
-	f.close()
 
-	## create document root
-	os.makedirs('../www/'+prj_name)
+s = s.replace('PRJ_NAME', prj_name)
+f = open(relative_prj_host, 'w')
+f.write(s)
+f.close()
 
-	## need restart docker-compose
-	print "Host %s succesfully created.\nNow run: docker-compose restart" % prj_name
+## create document root
+os.makedirs(document_root)
 
-	sys.exit(0)
+## need restart docker-compose
+print "Host %s succesfully created.\nNow run: docker-compose restart" % prj_name
 
-if len(sys.argv) == 3:
-	if '-v5' in sys.argv:
-		print "v5"
-		sys.exit(0)
-
-print "Incorrect arguments"
-print __doc__
-sys.exit(1)
+sys.exit(0)
 
