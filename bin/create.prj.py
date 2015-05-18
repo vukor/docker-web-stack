f89c10f1ea6b3e92361c4fac0feae0821149bc51
Anton Bugreev <anton@bugreev.ru>
2015-05-18 17:28:24 +0600

    add new functions

--- bin/create.prj.py
+++ bin/create.prj.py
@@ -9,8 +9,9 @@ Working on python version 2
 """
 
 import sys
-from subprocess import call
 import os
+import json
+import git
 
 ## checks on right argumnets
 if '-h' in sys.argv or '--help' in sys.argv:
@@ -40,38 +41,51 @@ elif len(sys.argv) == 3:
 	prj_name = sys.argv[2]
 	v5 = 1
 
+## check that config exists
+if not(os.path.isfile('./config.json')):
+    print "config.json does not exist! Exiting.."
+    sys.exit(1)
+
+
 ### main
 ## go to current dir with script
 os.chdir(os.path.dirname(sys.argv[0]))
 
-## create variables
-relative_prj_host = "../.nginx/etc/nginx/hosts/{}.conf".format(prj_name)
+## get prj_host, config values
+file=open('config.json').read()
+config = json.loads(file)
+host_name=config["host_name"].replace("{PRJ_NAME}", prj_name)
+dir_name=config["dir_name"].replace("{PRJ_NAME}", prj_name)
+relative_prj_host = "../.nginx/etc/nginx/hosts/{}.conf".format(host_name)
 
 ## exit if virtual host already exists
 if os.path.isfile(relative_prj_host):
 	print "prj host {} already exists! Exiting..".format(prj_name)
 	sys.exit(2)
 
-## exit if documentroot already exists
-if os.path.isdir('../htdocs/'+prj_name):
-	print 'documentroot {} already exists! Exiting..'.format(prj_name)
-	sys.exit(2)
+## if documentroot already exists
+if not(os.path.isdir('../htdocs/'+prj_name)):
+    if 'git_access' in file:
+        git.Git().clone(config["git_access"], '../htdocs/'+dir_name)
+    else:
+        os.makedirs('../htdocs/'+dir_name)
 
 ## create nginx config
 if v5 == 1:
-	document_root = "../htdocs/{}/www/".format(prj_name)
+	document_root = "../htdocs/{}/www/".format(host_name)
 	s = open('../.nginx/etc/nginx/hosts/template-v5-conf', 'r').read()
 else:
-	document_root = "../htdocs/{}/".format(prj_name)
+	document_root = "../htdocs/{}/".format(host_name)
 	s = open('../.nginx/etc/nginx/hosts/template-conf', 'r').read()
 
-s = s.replace('PRJ_NAME', prj_name)
+s = s.replace('HOST_NAME', host_name)
 f = open(relative_prj_host, 'w')
 f.write(s)
 f.close()
 
 ## create document root
-os.makedirs(document_root)
+if not(os.path.isdir(document_root)):
+    os.makedirs(document_root)
 
 ## finish
 print 'Vhost {} is created:\n\t- your config is {}\n\t- your documentroot is {}\n1. run: docker-compose restart nginx\n2. open http://{}/'.format(prj_name,os.path.normpath(os.path.realpath(relative_prj_host)), os.path.normpath(os.path.realpath(document_root)), prj_name)
