a5f6e618eadf098bb8ad786385e3c1f230ad6fb0
Anton Bugreev <anton@bugreev.ru>
2015-05-19 17:06:02 +0600

    ignore init-scripts

--- bin/create.prj.py
+++ bin/create.prj.py
@@ -1,77 +1,49 @@
 #!/usr/bin/env python
 
-"""
-Usage: create.prj.py [-v5] PRJNAME
-Args:
-	-v5: documentroot is htdocs/PRJNAME/www, otherwise is htdocs/PRJNAME
-
-Working on python version 2
-"""
-
 import sys
 import os
 import json
 import git
+import argparse
+import subprocess
 
-## checks on right argumnets
-if '-h' in sys.argv or '--help' in sys.argv:
-	print __doc__
-	sys.exit(1)
+## parse args
+parser = argparse.ArgumentParser("Create and init web project. Working on python 2.x")
+parser.add_argument('--config','-c', type=file, default='config.json', help='config file (default - config.json)')
+parser.add_argument('--version-lib','-v', type=int, choices=[4, 5], default=4, help='choose adx lib version (default - 4)')
+parser.add_argument('--init-script','-i', type=str, choices=['adx', 'wp'], help='run php script from init-scripts/')
+parser.add_argument('project', type=str)
+args = parser.parse_args()
 
-if not (2 <= len(sys.argv) <= 3):
-	print "Invalid arguments"
-	print __doc__
-	sys.exit(1)
-
-if len(sys.argv) == 3 and not(sys.argv[1] == '-v5'):
-	print "Invalid arguments"
-	print __doc__
-	sys.exit(1)
-	
-if len(sys.argv) == 2 and sys.argv[1] == '-v5':
-	print "Invalid arguments"
-	print __doc__
-	sys.exit(1)
-
-## save project name
-if len(sys.argv) == 2:
-	prj_name = sys.argv[1]
-	v5 = 0
-elif len(sys.argv) == 3:
-	prj_name = sys.argv[2]
-	v5 = 1
-
-## check that config exists
-if not(os.path.isfile('./config.json')):
-    print "config.json does not exist! Exiting.."
-    sys.exit(1)
-
+## init variables
+p = args.project
+v = args.version_lib
+f = args.config.read()
 
 ### main
 ## go to current dir with script
 os.chdir(os.path.dirname(sys.argv[0]))
 
 ## get prj_host, config values
-file=open('config.json').read()
-config = json.loads(file)
-host_name=config["host_name"].replace("{PRJ_NAME}", prj_name)
-dir_name=config["dir_name"].replace("{PRJ_NAME}", prj_name)
+config = json.loads(f)
+host_name=config["host_name"].replace("{PRJ_NAME}", p)
+dir_name=config["dir_name"].replace("{PRJ_NAME}", p)
 relative_prj_host = "../.nginx/etc/nginx/hosts/{}.conf".format(host_name)
 
 ## exit if virtual host already exists
 if os.path.isfile(relative_prj_host):
-	print "prj host {} already exists! Exiting..".format(prj_name)
+	print "prj host {} already exists! Exiting..".format(p)
 	sys.exit(2)
 
 ## if documentroot already exists
-if not(os.path.isdir('../htdocs/'+prj_name)):
-    if 'git_access' in file:
+if not(os.path.isdir('../htdocs/'+p)):
+    if 'git_access' in f:
         git.Git().clone(config["git_access"], '../htdocs/'+dir_name)
     else:
         os.makedirs('../htdocs/'+dir_name)
 
 ## create nginx config
-if v5 == 1:
+if v == 5:
 	document_root = "../htdocs/{}/www/".format(host_name)
 	s = open('../.nginx/etc/nginx/hosts/template-v5-conf', 'r').read()
 else:
@@ -87,8 +59,13 @@ f.close()
 if not(os.path.isdir(document_root)):
     os.makedirs(document_root)
 
+## run init-scripts
+if not(args.init_script == None):
+    subprocess.call(["php", "init-scripts/{}.php".format(args.init_script)])
+sys.exit(2)
+
 ## finish
-print 'Vhost {} is created:\n\t- your config is {}\n\t- your documentroot is {}\n1. run: docker-compose restart nginx\n2. open http://{}/'.format(prj_name,os.path.normpath(os.path.realpath(relative_prj_host)), os.path.normpath(os.path.realpath(document_root)), host_name)
+print 'Vhost {} is created:\n\t- your config is {}\n\t- your documentroot is {}\n1. run: docker-compose restart nginx\n2. open http://{}/'.format(p,os.path.normpath(os.path.realpath(relative_prj_host)), os.path.normpath(os.path.realpath(document_root)), host_name)
 
 sys.exit(0)
 
