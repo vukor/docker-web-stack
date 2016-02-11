f03fc2fa8895b7f3861c6c3f03ea61403301a887
Anton Bugreev <anton@bugreev.ru>
2016-02-11 16:26:27 +0600

    Добавлен dns

--- bin/images.push.sh
+++ bin/images.push.sh
@@ -4,7 +4,7 @@ DIR=`dirname "$0"`
 cd $DIR
 
 ## build images
-for image in postfix mysql55 php54 php56 php70 nginx; do
+for image in postfix mysql55 php54 php56 php70 nginx dns; do
 	docker push "vukor/$image"
 done
 
