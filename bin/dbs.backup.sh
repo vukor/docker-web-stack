646b43327b8fe754cc975d45909b8ed696425018
Anton Bugreev <anton@bugreev.ru>
2016-05-08 13:53:54 +0600

    Fix scripts for run in docker vukor/cron

--- bin/dbs.backup.sh
+++ bin/dbs.backup.sh
@@ -7,7 +7,7 @@ cd ${dir}/../ || exit 2
 
 ## Backup all your databases:
 echo "*** Backing up all your databases.. ***"
-docker-compose run --rm mysql bash -c 'exec /opt/backup.dbs.sh' || exit 1
+docker-compose exec mysql bash -c 'exec /opt/backup.dbs.sh' || exit 1
 echo "** Backing up done. *** "
 
 exit 0
--- bin/dbs.restore.sh
+++ bin/dbs.restore.sh
@@ -8,7 +8,7 @@ cd ${dir}/../ || exit 2
 ## Restore all your databases:
 echo "*** Restoring all your databases.. ***"
 sleep 1
-docker-compose run --rm mysql bash -c 'exec /opt/restore.dbs.sh' || exit 1
+docker-compose exec mysql bash -c 'exec /opt/restore.dbs.sh' || exit 1
 echo "*** Restoring databases done. ***"
 
 exit 0
